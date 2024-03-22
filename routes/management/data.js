7c7698a85445b537db7bb7ff7996b02a705464cd
RG-04 <118968517+RG-04@users.noreply.github.com>
2024-03-22 17:01:23 +0530

    more minor fix

--- routes/management/data.js
+++ routes/management/data.js
@@ -61,12 +61,11 @@ exports.customers = async (req, res, next) => {
 
 exports.customerById = async (req, res, next) => {
   // Customer info route
-  let customer = await Customer.find({ uid: req.params.id });
-
+  let customer = await Customer.findOne({ uid: req.params.id });
   if (!customer) {
     return res.status(404).json({ error: "Customer not found" });
   }
-
+  
   let resJson = await formatCustomer(customer, {
     showAddress: true,
     showPhone: true,
